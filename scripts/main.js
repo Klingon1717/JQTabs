88ac9b744131d6e6656149e410a6329057528dc8
Whitney Murray <wnksmith92@yahoo.com>
2015-10-06 23:16:50 -0500

    normal mode down

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+$(document).ready(function() {
+	$(window.location.hash).show();
+	$('a[href="'+window.location.hash+'"]').addClass('active');
+
+
+
+	$('nav').click(function(e) {
+		
+		
+		var $target = $(e.target);
+		var href = $target.attr('href');
+		$('a').removeClass('active');
+		$target.addClass('active');
+		$('section').hide();
+		$(href).show();
+		$(this).hide();
+
+	});
+});
+
